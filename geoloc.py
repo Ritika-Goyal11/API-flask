443a9cbc59cde81c1150d370e803a943c259b874
Ritika-Goyal11 <goyalritika3425@gmail.com>
2024-10-20 00:30:38 +0545

    First Commit

--- geoloc.py
+++ geoloc.py
@@ -4,34 +4,17 @@ import os
 
 app = Flask(__name__)
 
-print(os.environ.get('API_KEY'))
+# print(os.getenv(''))
 api_key = os.environ.get('API_KEY')
 
 api_endpoint = "https://api.ipgeolocation.io/ipgeo"
 
-def extract_location_data(json_data):
-    location_info = {
-        "ip":json_data["ip"],
-        "latitude": json_data["latitude"],
-        "longitude": json_data["longitude"],
-        "city": json_data["city"],
-        "state": json_data["state_prov"],
-        "country": json_data["country_name"],
-        "zip_code": json_data["zipcode"],
-        "country_flag": json_data["country_flag"],
-        "current_time": json_data['time_zone']['current_time'],
-        "time_zone":json_data['time_zone']['name']
-    }
-    return location_info
-
 @app.route('/')
 def get_geolocation():
-
     params = {
         'apiKey': api_key,
         'ip': ''    # IP can be left blank to get our current IP
     }
-
     ip = request.args.get('ip')
     if ip:
         params['ip']=ip
@@ -40,16 +23,18 @@ def get_geolocation():
     
     if response.status_code == 200:
         data = response.json()
-        return render_template('home.html',data=extract_location_data(data))
+        print(data)
+        return render_template('home.html',data=data)
     else:
-        return f"Error: {response.status_code}, {response.text}"
+        error_data = response.json()
+        return render_template('error.html', error_code=response.status_code, error_message='IP does not exist.')
 
-@app.route('/inp')
-def index():
+@app.route('/input')
+def input_ip():
     return render_template('input.html')
 
 @app.route('/', methods=['POST'])
-def h():
+def index():
     ip = request.form.get('ip')
     return redirect(url_for('get_geolocation', ip=ip))
 
